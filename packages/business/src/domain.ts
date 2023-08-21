f3da855cd8983add6c584fa9174aec3438087234
sylvain <fraisse.sylvain@gmail.com>
2023-08-21 11:58:44 +0200

    :label: (business) ClinicalTrial definition

---
+++ packages/business/src/domain.ts
@@ -0,0 +1,25 @@
+const COUNTRIES = {
+  AT: "Austria",
+  DE: "Germany",
+  ES: "Spain",
+  FR: "France",
+  IT: "Italy",
+} as const
+
+type CountryCode = keyof typeof COUNTRIES
+type CountryName = typeof COUNTRIES[CountryCode]
+
+type Country = {
+  code: CountryCode
+  name: CountryName
+}
+
+type ClinicalTrial = {
+  name: string
+  country: Country
+  sponsor: string
+  startDate: Date
+  endDate: Date
+}
+
+export { COUNTRIES, CountryCode, CountryName, ClinicalTrial }
