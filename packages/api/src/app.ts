1a395030fcd175381979e26bd73e86fb0cae8a3d
sylvain <fraisse.sylvain@gmail.com>
2023-09-06 08:39:17 +0200

    :sparkles: (api) basic error handler
    An error handler is plugged into the express app.
    It distinguishes between an identified `BusinessError` that sends a 400 status code and the other errors that send a classical 500 status code.

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -2,11 +2,13 @@ import express, { Response } from 'express'
 
 import { Adapters } from './types'
 import { checkValidity, getOngoingClinicalTrialsHandler } from './handlers/get-ongoing-clinical-trials-handler'
+import { errorMiddleware } from './middlewares/error-handler'
 
 const createApp = (adapters: Adapters) =>
   express()
-  .get('/ping', (_req, res: Response) =>  res.send('pong'))
-  .get('/on-goings', checkValidity, getOngoingClinicalTrialsHandler(adapters))
+    .get('/ping', (_req, res: Response) =>  res.send('pong'))
+    .get('/on-goings', checkValidity, getOngoingClinicalTrialsHandler(adapters))
+    .use(errorMiddleware)
 
 const startApp = (adapters: Adapters, port = 8080) =>
   createApp(adapters).listen(port, () => console.log(`Server running on port ${port} 🍺`))
